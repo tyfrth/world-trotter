3cdf859a8681189b27a932aa23b1375a28ebcbd4
tyfrth <tyfrith@gmail.com>
2018-06-11 12:31:29 -0700

    constraints an such

--- WorldTrotter/MapViewController.swift
+++ WorldTrotter/MapViewController.swift
@@ -21,6 +21,28 @@ class MapViewController: UIViewController {
         
         //set it as *th* view of this view controller
         view = mapView
+        
+        //create our segmented control
+        let segmentedControl = UISegmentedControl(items: ["Standard", "Hybrid", "Satellite"])
+        segmentedControl.backgroundColor = UIColor.white
+        segmentedControl.selectedSegmentIndex = 0
+        
+        segmentedControl.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(segmentedControl)
+        
+        //adding constraints programmatically
+        let topConstraint = segmentedControl.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 10)
+        
+        let margins = view.layoutMarginsGuide
+        let leadingConstraint = segmentedControl.leadingAnchor.constraint(equalTo: margins.leadingAnchor)
+        let trailingConstraint = segmentedControl.trailingAnchor.constraint(equalTo: margins.trailingAnchor)
+        
+        //activate constraints
+        topConstraint.isActive = true
+        leadingConstraint.isActive = true
+        trailingConstraint.isActive = true
+        
+        
     }
     
     override func viewDidLoad() {
